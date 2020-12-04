a9f15ad442e9deaa80f642bea6d5a0499f3297e4
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2020-12-04 16:05:29 +0200

    Fixed wrong declaration of func

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,18 +1,19 @@
 package greetings
 
 import (
-	"fmt"
-	"errors"
+    "errors"
+    "fmt"
 )
 
-// Hello returns a greeting to a named person
-func Hello(name string) string {
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
     // If no name was given, return an error with a message.
-	if name == "" {
-		return '', errors.New("name is empty")
-	}
+    if name == "" {
+        return "", errors.New("empty name")
+    }
 
-    // Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message, nil
+    // If a name was received, return a value that embeds the name 
+    // in a greeting message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message, nil
 }
