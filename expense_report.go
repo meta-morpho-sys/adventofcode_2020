9029cba47d1cbd72bf539c8c608a57d7acd0fd2a
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2020-12-08 16:56:24 +0200

    Initial setup

---
+++ expense_report.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// expenses := []int{1721
+// 	979
+// 	366
+// 	299
+// 	675
+// 	1456}
+
+func main() {
+	expense := ShowExpenses()
+	fmt.Println(expense)
+}
+
+func ShowExpenses() (expense int) {
+	sum := int(1 + 1)
+	return sum
+}
