74630c626699241557f99a441b413fe849898688
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2020-12-10 17:01:21 +0200

    Store result in a new slice

--- expense_report.go
+++ expense_report.go
@@ -18,15 +18,15 @@ var expensesInput = []int{
 }
 
 func main() {
-	expense, _ := find2020(expensesInput, 2020)
+	expense := find2020(expensesInput)
 	fmt.Println(expense)
 }
 
 // Take a list of expenses and check which two make the sum of 2020
-func find2020(expenses []int, target int) (int, int) {
-	if len(expenses) <= 1 {
-		return -1, -1
-	}
+func find2020(expenses []int) (found[]int) {
+	// if len(expenses) <= 1 {
+	// 	return -1, nil
+	// }
 	// n = 0 // Where n is the index where the value that we need to sum up is stored
 	// result = xn + x(n+1)
 	//	Take the n0 and sum it up with the values of indices "n" (by iterating over them?)
@@ -35,9 +35,13 @@ func find2020(expenses []int, target int) (int, int) {
 
 	for i := 0; i < len(expenses); i++ {
 		result := expenses[0] + expenses[i]
-		fmt.Println("i is :", expenses[0], "next i is: ", expenses[i])
+		fmt.Println("\ni is :", expenses[0], "next i is: ", expenses[i])
 		fmt.Println("Result is: ", result)
+		found := []int{}
+		if result == 2020 {
+			found = append(found, expenses[0], expenses[i])
+			fmt.Printf("These values %v sum up to 2020",found)
+		}
 	}
-
-	return -1, -1
+	return found
 }
